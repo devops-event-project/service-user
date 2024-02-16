6c6507afe76cd864281d0844cb7681748bcb0479
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-02-16 16:58:05 +0100

    Grouped users and CRUD operations
    Grouped users and CRUD operations

--- routes/user.py
+++ routes/user.py
@@ -7,24 +7,24 @@ from bson import ObjectId
 
 user = APIRouter()
 
-@user.get('/')
+@user.get('/user/', tags=["Get Methods"])
 async def find_all_users():
     return serializeList(conn.local.user.find())
 
-@user.get('/{id}')
+@user.get('/user/{id}', tags=["Get Methods"])
 async def fine_one_user(id):
     return serializeDict(conn.local.user.find_one({"_id":ObjectId(id)}))
 
-@user.post('/')
+@user.post('/user/', tags=["Post Methods"])
 async def create_user(user: User): # Which we have created in models
     result = conn.local.user.insert_one(dict(user))
     return serializeDict(conn.local.user.find_one({"_id":result.inserted_id}))
 
-@user.put('/{id}')
+@user.put('/user/{id}', tags=["Put Methods"])
 async def update_user(id, user: User):
     conn.local.user.find_one_and_update({"_id":ObjectId(id)},{"$set":dict(user)})
     return serializeDict(conn.local.user.find_one({"_id":ObjectId(id)}))
 
-@user.delete('/{id}')
+@user.delete('/user/{id}', tags=["Delete Methods"])
 async def delete_user(id):
     return serializeDict(conn.local.user.find_one_and_delete({"_id":ObjectId(id)}))
--- schemas/user.py
+++ schemas/user.py
@@ -1,14 +1,14 @@
-def userEntity(item) -> dict:
-	return {
-		"id":str(item["_id"]),
-		"name":str(item["name"]),
-		"email":str(item["email"]),
-		"password":str(item["password"]),
+# def userEntity(item) -> dict:
+# 	return {
+# 		"id":str(item["_id"]),
+# 		"name":str(item["name"]),
+# 		"email":str(item["email"]),
+# 		"password":str(item["password"]),
 
-	}
+# 	}
 
-def usersEntity(items) -> list:
-	return [userEntity(item) for item in items]
+# def usersEntity(items) -> list:
+# 	return [userEntity(item) for item in items]
 
 def serializeDict(item) -> dict:
 	return {**{i:str(item[i]) for i in item if i=='_id'},**{i:item[i] for i in item if i!='_id'}}
