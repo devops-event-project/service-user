0aa70bc27fcf2f74265770784bb9356f5d1ffcce
Anandan Krishnsamy <44828456+anandanlk@users.noreply.github.com>
2024-02-18 13:02:59 +0100

    DB url updated

--- config/db.py
+++ config/db.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
 
 # Connecting to Database
-conn = MongoClient("mongodb://localhost:27017/user")
+# conn = MongoClient("mongodb://localhost:27017")
+conn = MongoClient("mongodb://mongo:27017/local")
